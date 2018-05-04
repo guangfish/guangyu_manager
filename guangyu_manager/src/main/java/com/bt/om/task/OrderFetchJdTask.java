a2c7126d63f4fffc2e3166a298c41a873ff74a19
aineness <aineness@163.com>
2018-05-04 21:13:53 +0800

    1

--- guangyu_manager/src/main/java/com/bt/om/task/OrderFetchJdTask.java
+++ guangyu_manager/src/main/java/com/bt/om/task/OrderFetchJdTask.java
@@ -73,8 +73,8 @@ public class OrderFetchJdTask {
 		driver.manage().timeouts().implicitlyWait(1500, TimeUnit.MILLISECONDS);
 	}
 
-	@Scheduled(cron = "0 0 */1 * * ?")
-//	@Scheduled(cron = "0 0/2 * * * ?")
+//	@Scheduled(cron = "0 0 */1 * * ?")
+	@Scheduled(cron = "0 0/2 * * * ?")
 	public void orderFetch() {
 		logger.info("京东订单报表下载入库");
 		try {
--- guangyu_manager/src/main/java/com/bt/om/task/OrderFetchTask.java
+++ guangyu_manager/src/main/java/com/bt/om/task/OrderFetchTask.java
@@ -79,8 +79,8 @@ public class OrderFetchTask {
 		driver.manage().timeouts().implicitlyWait(1500, TimeUnit.MILLISECONDS);
 	}
 
-	@Scheduled(cron = "0 0 0/1 * * ?")
-//	@Scheduled(cron = "0 0/2 * * * ?")
+//	@Scheduled(cron = "0 0 0/1 * * ?")
+	@Scheduled(cron = "0 0/4 * * * ?")
 	public void orderFetch() {
 		logger.info("淘宝订单报表下载入库");
 		try {
