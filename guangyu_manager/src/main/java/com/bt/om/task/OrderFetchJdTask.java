67f899365fbe68f3a669d917bb785bee6e63ddf6
aineness <aineness@163.com>
2018-08-03 10:27:10 +0800

    1

--- guangyu_manager/src/main/java/com/bt/om/task/OrderFetchJdTask.java
+++ guangyu_manager/src/main/java/com/bt/om/task/OrderFetchJdTask.java
@@ -91,8 +91,8 @@ public class OrderFetchJdTask {
 //		driver.manage().timeouts().implicitlyWait(1500, TimeUnit.MILLISECONDS);
 //	}
 
-//	@Scheduled(cron = "0 0/1 * * * ?")
-	@Scheduled(cron = "0 0/20 7-23 * * ?")
+	@Scheduled(cron = "0 0/2 * * * ?")
+//	@Scheduled(cron = "0 0/20 7-23 * * ?")
 	public void orderJdFetchTask() {
 		String ifRun = GlobalVariable.resourceMap.get("OrderFetchJdTask");
 		if ("1".equals(ifRun)) {
@@ -114,14 +114,14 @@ public class OrderFetchJdTask {
 //			driver.navigate().refresh();
 //			Thread.sleep(NumberUtil.getRandomNumber(sleepTimeBegin * 2, sleepTimeEnd * 2));
 			
-//			try {
-//				WebElement element0 = driver.findElement(By.xpath("/html/body/div[7]/div[1]/div[2]/a"));
-//				PageUtils.scrollToElementAndClick(element0, driver);
-//				Thread.sleep(NumberUtil.getRandomNumber(sleepTimeBegin, sleepTimeEnd));
-//			} catch (Exception e) {
-////				e.printStackTrace();
-//				System.out.println("京东右下角浮动窗口已关闭");
-//			}
+			try {
+				WebElement element0 = driver.findElement(By.xpath("/html/body/div[7]/div[1]/div[2]/a"));
+				PageUtils.scrollToElementAndClick(element0, driver);
+				Thread.sleep(NumberUtil.getRandomNumber(sleepTimeBegin, sleepTimeEnd));
+			} catch (Exception e) {
+//				e.printStackTrace();
+				System.out.println("京东右下角浮动窗口已关闭");
+			}
 
 			// 点击快捷日期
 			WebElement element = driver.findElement(By.xpath("//*[@id='inShortcutDate']"));
