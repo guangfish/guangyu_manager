5abcad2991b79566861a1ad84bb65f4da8a443ce
aineness <aineness@163.com>
2018-08-22 19:04:52 +0800

    1

--- guangyu_manager/src/main/java/com/bt/om/web/controller/app/AppBannerController.java
+++ guangyu_manager/src/main/java/com/bt/om/web/controller/app/AppBannerController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.adtime.common.lang.StringUtil;
 import com.bt.om.entity.Banner;
 import com.bt.om.service.IBannerService;
 import com.bt.om.web.BasicController;
@@ -31,24 +32,38 @@ public class AppBannerController extends BasicController {
 
 	@RequestMapping(value = "/banner", method = RequestMethod.POST)
 	@ResponseBody
-	public Model list(Model model, HttpServletRequest request, HttpServletResponse response) {		
+	public Model list(Model model, HttpServletRequest request, HttpServletResponse response) {
+		String ua = request.getHeader("User-Agent");
+		String showLink = "yes";
+		if (StringUtil.isNotEmpty(ua)) {
+			ua = ua.toLowerCase();
+			if (ua.contains("android")) {
+				showLink = "yes";
+			} else {
+				showLink = "no";
+			}
+		}
 		BannerVo bannerVo = new BannerVo();
 		bannerVo.setDesc("获取成功");
 		bannerVo.setStatus("0");
 		List<Banner> bannerList = bannerService.selectForApp(1);
-		if(bannerList!=null && bannerList.size()>0){			
-			List<Map<String, String>> list=new ArrayList<>();
-			for(Banner banner:bannerList){
-				Map<String,String> map=new HashMap<>();
+		if (bannerList != null && bannerList.size() > 0) {
+			List<Map<String, String>> list = new ArrayList<>();
+			for (Banner banner : bannerList) {
+				Map<String, String> map = new HashMap<>();
 				map.put("imgUrl", banner.getImgUrl());
-				map.put("link", banner.getLink());
-				map.put("title", banner.getTitle()==null?"":banner.getTitle());
+				if ("yes".equals(showLink)) {
+					map.put("link", banner.getLink());
+				} else {
+					map.put("link", "");
+				}
+				map.put("title", banner.getTitle() == null ? "" : banner.getTitle());
 				map.put("width", banner.getWidth());
 				map.put("height", banner.getHight());
 				list.add(map);
 			}
 			bannerVo.setData(list);
-		}			
+		}
 		model.addAttribute("response", bannerVo);
 		return model;
 	}
