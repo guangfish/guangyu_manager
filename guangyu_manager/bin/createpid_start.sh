f2d63714c2c9bddd06e0d95c8bac6334432cc8c7
aineness <aineness@163.com>
2018-12-06 15:23:23 +0800

    1

--- guangyu_manager/bin/createpid_start.sh
+++ guangyu_manager/bin/createpid_start.sh
@@ -2,8 +2,15 @@
 
 PRGDIR=`dirname $0`
 SERVER_HOME=$(echo `readlink -f $PRGDIR` | sed 's/\/bin//')
-. "$SERVER_HOME/bin/env.sh"
+
+CLASSPATH=`echo $JAVA_HOME/lib/*.jar | tr ' ' ':'`
+CLASSPATH=$CLASSPATH:`echo $SERVER_HOME/target/lib/compile/*.jar | tr ' ' ':'`
+CLASSPATH=$CLASSPATH:$SERVER_HOME/conf
+if [ -d "$SERVER_HOME/target/src/classes" ] ; then
+	CLASSPATH=$SERVER_HOME/target/src/classes:$CLASSPATH
+fi
 
 export DISPLAY=:1
 export CLASSPATH=$CLASSPATH
-nohup $JAVA_HOME/bin/java -server $JAVA_OPTS com.bt.om.CreateTaobaoPid ${COMMAND} > $SERVER_HOME/logs/catalina-pids.log 2>&1 &
+
+nohup ${JAVA_HOME}/bin/java com.bt.om.CreateTaobaoPid >${SERVER_HOME}/logs/catalina-pids.log 2>&1 &
